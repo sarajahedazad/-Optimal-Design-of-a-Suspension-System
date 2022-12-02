280228db1d0fa066f983d8fd050f1e937a68482b
Sara Jahedazad <114187084+sarajahedazad@users.noreply.github.com>
2022-12-02 15:29:55 +0330

    Add files via upload

---
+++ MATLAB_files/HW6_7_co_fraction.m
@@ -0,0 +1,11 @@
+close all;clear all;clc;
+nvars=8;
+A=[];b=[];Aeq=[];beq=[];
+LB=[500,500,500,500,5000,5000,90000,90000];
+UB=[2000,2000,2000,2000,20000,20000,150000,150000];
+nonlcon=[];
+
+options = optimoptions('ga','PlotFcn', @gaplotbestf, 'CrossoverFraction', 0.3);
+out= ga(@objfun,nvars,A,b,Aeq,beq,LB,UB,nonlcon,options);
+axis 'auto x'
+out
---
+++ MATLAB_files/HW6_9_fun_tol.m
@@ -0,0 +1,13 @@
+close all;clear all;clc;
+nvars=8;
+A=[];b=[];Aeq=[];beq=[];
+LB=[500,500,500,500,5000,5000,90000,90000];
+UB=[2000,2000,2000,2000,20000,20000,150000,150000];
+nonlcon=[];
+
+options = optimoptions('ga','PlotFcn', @gaplotbestf,'FunctionTolerance', 0.001);
+out= ga(@objfun,nvars,A,b,Aeq,beq,LB,UB,nonlcon,options);
+axis 'auto x'
+out
+
+
